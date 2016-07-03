2c8ccda67460ce1c9b69c65fe8f475cc4650ed65
Pablo Gauna <pablogauna@msn.com>
2016-07-03 20:04:24 -0300

    Add Draw Circle

--- PadTransposer.rb
+++ PadTransposer.rb
@@ -2,16 +2,39 @@ require "midi"
 
 # prompt the user to select an input and output
 
-input = UniMIDI::Input.gets
-output = UniMIDI::Output.gets
 
-MIDI.using(input, output) do
+class PadTransposer
+  def initialize
 
-  thru_except :note do |message|
-    #message.note += 12
-    output(message)
+    drawCircle
   end
 
-  join
+  def drawCircle
+    input = UniMIDI::Input.find_by_name 'IK Multimedia iRig KEYS Pro'
+    output = UniMIDI::Output.find_by_name 'Focusrite A.E. Ltd Launchpad S'
 
+    MIDI.using(input, output) do
+      speed = 127
+      output(MIDIMessage::NoteOn["E-1"].new(0, speed))
+      output(MIDIMessage::NoteOn["D#-1"].new(0, speed))
+      output(MIDIMessage::NoteOn["A0"].new(0, speed))
+      output(MIDIMessage::NoteOn["F#0"].new(0, speed))
+      output(MIDIMessage::NoteOn["A1"].new(0, speed))
+      output(MIDIMessage::NoteOn["D2"].new(0, speed))
+      output(MIDIMessage::NoteOn["C3"].new(0, speed))
+      output(MIDIMessage::NoteOn["G3"].new(0, speed))
+      output(MIDIMessage::NoteOn["E4"].new(0, speed))
+      output(MIDIMessage::NoteOn["B4"].new(0, speed))
+      output(MIDIMessage::NoteOn["A5"].new(0, speed))
+      output(MIDIMessage::NoteOn["D6"].new(0, speed))
+      output(MIDIMessage::NoteOn["D7"].new(0, speed))
+      output(MIDIMessage::NoteOn["F7"].new(0, speed))
+      output(MIDIMessage::NoteOn["G8"].new(0, speed))
+      output(MIDIMessage::NoteOn["G#8"].new(0, speed))
+    end
+  end
 end
+
+transposer = PadTransposer.new
+
+
